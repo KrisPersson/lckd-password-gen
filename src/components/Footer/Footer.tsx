931b77e6f8d5619d150da46250dcb62545e014ba
Marvan Al Kufai <alkufai@gmail.com>
2023-10-19 11:07:23 +0200

    feat: add create item view fix: small changes to button props

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,14 @@
 import './Footer.scss';
 import Button from '../Button/Button';
 interface Props {
-  isLoggedIn: boolean;
+  buttonText: string;
+  gatherData: () => void;
 }
 
 export function Footer(props: Props) {
   return (
     <div className="footer">
-      <Button
-        width="8.6rem"
-        height="3.8rem"
-        radius="0.3rem"
-        border="solid 0.1rem #ff4910"
-        backgroundColor="transparent"
-        color="#ff4910"
-        onClick={() => console.log('Login')}
-      >
-        Sign up
-      </Button>
+      <Button onClick={props.gatherData}>{props.buttonText}</Button>
     </div>
   );
 }
