7bd7bfa6259987024d847b35b07b0645a14e96d8
Marvan Al Kufai <alkufai@gmail.com>
2023-10-20 11:51:55 +0200

    feat: add edit item view

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,27 +1,53 @@
 import { DatabaseSavedItem as Item } from '../types/index';
 import { v4 as uuidv4 } from 'uuid';
 
+const htmlEl = {
+  www: document.getElementById('WWW') as HTMLInputElement,
+  username: document.getElementById('USERNAME') as HTMLInputElement,
+  password: document.getElementById('SECURE PASSWORD') as HTMLInputElement,
+};
+
 function getDataFromInputsAndSave() {
-  const www = document.getElementById('WWW') as HTMLInputElement;
-  const username = document.getElementById('USERNAME') as HTMLInputElement;
-  const password = document.getElementById('SECURE PASSWORD') as HTMLInputElement;
-  if (www.value.length < 8 || !username.value || !password.value) {
+  if (!htmlEl.www.value || !htmlEl.username.value || !htmlEl.password.value) {
     return console.log('error');
   } else {
     const id = uuidv4();
     const itemToSaveToDatabase: Item = {
-      savedUrl: www.value,
-      savedUsername: username.value,
-      savedPassword: password.value,
+      savedUrl: htmlEl.www.value,
+      savedUsername: htmlEl.username.value,
+      savedPassword: htmlEl.password.value,
       id: id,
     };
     writeToDatabase(itemToSaveToDatabase);
   }
 }
-
+async function fillInputsFromDb(itemId: string) {
+  // Hämta data från databasen med hjälp av itemId
+  // Skriv ut data i inputs
+  htmlEl.www.value = '';
+  htmlEl.username.value = '';
+  htmlEl.password.value = '';
+}
+function getDataFromInputsAndUpdate(itemId: string) {
+  if (htmlEl.www.value.length < 8 || !htmlEl.username.value || !htmlEl.password.value) {
+    return console.log('error');
+  } else {
+    const itemToUpdateInDb: Item = {
+      savedUrl: htmlEl.www.value,
+      savedUsername: htmlEl.username.value,
+      savedPassword: htmlEl.password.value,
+      id: itemId,
+    };
+    updateDatabase(itemToUpdateInDb);
+  }
+}
+function updateDatabase(itemToUpdateInDb: Item) {
+  // skicka till api endpoint
+  return console.log(itemToUpdateInDb);
+}
 function writeToDatabase(itemToWriteToDb: Item) {
   // skicka till api endpoint
   return console.log(itemToWriteToDb);
 }
 
-export { getDataFromInputsAndSave };
+export { getDataFromInputsAndSave, getDataFromInputsAndUpdate, fillInputsFromDb };
--- src/views/EditItemView/EditItemView.tsx
+++ src/views/EditItemView/EditItemView.tsx
@@ -1,10 +1,24 @@
 import './EditItemView.scss';
 import { Header } from '../../components/Header/Header';
+import { Footer } from '../../components/Footer/Footer';
+import { getDataFromInputsAndUpdate, fillInputsFromDb } from '../../utils/helpers';
+import Input from '../../components/Input/Input';
 
 export function EditItemView() {
+  const itemId: string = '1'; //temporärt
+  fillInputsFromDb(itemId);
   return (
     <div className="view">
       <Header isLoggedIn={true} />
+      <div className="edit-item-view__content">
+        <h1 className="edit-item-view__header">EDIT YOUR CREDENTIALS</h1>
+        <div className="edit-item-view__inputs">
+          <Input label="WWW" htmlFor="WWW" />
+          <Input label="USERNAME" htmlFor="USERNAME" />
+          <Input label="SECURE PASSWORD" htmlFor="SECURE PASSWORD" fieldType="genPwd" />
+        </div>
+      </div>
+      <Footer buttonText="UPDATE LCKD" onClick={() => getDataFromInputsAndUpdate(itemId)} />
     </div>
   );
 }
